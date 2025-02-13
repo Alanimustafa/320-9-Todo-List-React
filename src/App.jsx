2a64d600f27c2b57a2f14ea820fed00be2c7c737
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-13 17:51:26 -0600

    Moving the todoReducer function in a saperated module

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import { useState, useReducer } from 'react'
 import { initialState } from './Data/data'
+import todoReducer from './TodoReducer'
 import './App.css'
 
 function TodoList() {
 
-
-  // The todoReducer function to update the state according to the action types
-  function todoReducer(state, action) {
-
-    //Creating sswtiches for the action cases 
-    switch (action.type) {
-      case "ADD_TODO" : return [{ id: Date.now(), title: action.payload, completed: false, editing: false }, ...state];
-      case "CHECKED_COMPLETE" : return state.map(todo => todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo); // Looping through the state to mark completion of the current todo.
-      case "DELETE_TODO" : return state.filter(todo => todo.id !== action.payload); // Deleting the selected Todo using the id
-      case "EDIT_TODO" : return state.map(todo => todo.id === action.payload ? { ...todo, editing: true } : todo);
-      case "SAVE_TODO" : return state.map(todo => todo.id === action.payload.id ? { ...todo, text: action.payload.text, editing: false } : todo);
-      default:
-        return state;
-    }
-    
-  }
 
 // Using the useReducer to Manage the State of the list revoked from the data.
 const [todos, dispatch] = useReducer(todoReducer, initialState)
